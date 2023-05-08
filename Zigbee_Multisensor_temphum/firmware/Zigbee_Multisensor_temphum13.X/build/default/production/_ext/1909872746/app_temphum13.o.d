7fcb3693ff3b3523a4f05119af402b310f42c932
Roopika M M <Roopika.MM@microchip.com>
2023-05-08 15:47:24 +0530

    MetaData added

---
+++ Zigbee_Multisensor_temphum/firmware/Zigbee_Multisensor_temphum13.X/build/default/production/_ext/1909872746/app_temphum13.o.d
@@ -0,0 +1,66 @@
+build/default/production/_ext/1909872746/app_temphum13.o: \
+ ../src/app_tempHum13/app_temphum13.c \
+ ../src/config/default/zigbee/lib/Zigbee_AllDevice_Makerules.h \
+ ../src/config/default/zigbee/z3device_configs/zigbeeAppConfig.h \
+ ../src/config/default/zigbee/z3device_configs/zigbeeAppDeviceSelect.h \
+ ../src/config/default/zigbee/z3device_configs/stackConfig.h \
+ ../src/app_tempHum13/app_temphum13.h ../src/config/default/interrupts.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/FreeRTOS.h \
+ ../src/config/default/FreeRTOSConfig.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/projdefs.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/portable.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../src/third_party/rtos/FreeRTOS/Source/portable/GCC/SAM/ARM_CM4F/portmacro.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/mpu_wrappers.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/task.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/list.h \
+ ../src/config\default\peripheral\sercom\i2c_master\plib_sercom2_i2c_master.h \
+ ../src/config\default\peripheral\sercom\i2c_master\plib_sercom_i2c_master_common.h \
+ ../src/config/default/device.h ../src/config/default/device_cache.h \
+ ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_compiler.h
+
+../src/config/default/zigbee/lib/Zigbee_AllDevice_Makerules.h:
+
+../src/config/default/zigbee/z3device_configs/zigbeeAppConfig.h:
+
+../src/config/default/zigbee/z3device_configs/zigbeeAppDeviceSelect.h:
+
+../src/config/default/zigbee/z3device_configs/stackConfig.h:
+
+../src/app_tempHum13/app_temphum13.h:
+
+../src/config/default/interrupts.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/FreeRTOS.h:
+
+../src/config/default/FreeRTOSConfig.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/projdefs.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/portable.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/deprecated_definitions.h:
+
+../src/third_party/rtos/FreeRTOS/Source/portable/GCC/SAM/ARM_CM4F/portmacro.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/mpu_wrappers.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/task.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/list.h:
+
+../src/config\default\peripheral\sercom\i2c_master\plib_sercom2_i2c_master.h:
+
+../src/config\default\peripheral\sercom\i2c_master\plib_sercom_i2c_master_common.h:
+
+../src/config/default/device.h:
+
+../src/config/default/device_cache.h:
+
+../src/config/default/device.h:
+
+../src/config/default/toolchain_specifics.h:
+
+../src/packs/CMSIS/CMSIS/Core/Include/cmsis_compiler.h:
---
+++ Zigbee_Multisensor_temphum/firmware/Zigbee_Multisensor_temphum13.X/build/default/production/_ext/410253077/commissioningCluster.o.d
@@ -0,0 +1,14 @@
+build/default/production/_ext/410253077/commissioningCluster.o: \
+ ../src/config/default/zigbee/z3device/clusters/commissioningCluster.c \
+ ../src/config/default/zigbee/lib/Zigbee_AllDevice_Makerules.h \
+ ../src/config/default/zigbee/z3device_configs/zigbeeAppConfig.h \
+ ../src/config/default/zigbee/z3device_configs/zigbeeAppDeviceSelect.h \
+ ../src/config/default/zigbee/z3device_configs/stackConfig.h
+
+../src/config/default/zigbee/lib/Zigbee_AllDevice_Makerules.h:
+
+../src/config/default/zigbee/z3device_configs/zigbeeAppConfig.h:
+
+../src/config/default/zigbee/z3device_configs/zigbeeAppDeviceSelect.h:
+
+../src/config/default/zigbee/z3device_configs/stackConfig.h:
